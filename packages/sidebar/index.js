03bb491d5a58422e247ee2b3385db7ec34e3106f
Benjamin Akar <benjaminakar2001@gmail.com>
2021-09-15 15:12:25 +0200

    Started building the sidebar

--- packages/sidebar/index.js
+++ packages/sidebar/index.js
@@ -2,56 +2,85 @@ class FabricDocsSidebar extends HTMLElement {
   constructor() {
     super();
 
-    const fabricStylesTemplate = document.createElement("template");
+    const fabricStylesTemplate = document.createElement('template');
     fabricStylesTemplate.innerHTML = `
-            <style>:host { display: block; }</style>
-            <link
-                rel="stylesheet"
-                type="text/css"
-                href="https://assets.finn.no/pkg/@finn-no/fabric-css/v0/fabric.min.css"
-            />
-            <section>
-              <input type="text" placeholder="Search">
-              <nav>
-                <slot></slot>
-              </nav>
-            </section>
-        `;
+      <style>:host { display: block; }</style>
+      <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://assets.finn.no/pkg/@finn-no/fabric-css/v0/fabric.min.css"
+      />
+      <section style="width: 250px;" class="bg-gray-50 h-screen p-20">
+        <div class="input px-6">
+          <input type="text" placeholder="Search">
+        </div>
+        <nav id="fabric-docs-sidebar" aria-orientation="vertical"></nav>
+      </section>
+  `;
 
-    this.attachShadow({ mode: "open" }).appendChild(
+    this.attachShadow({ mode: 'open' }).appendChild(
       fabricStylesTemplate.content
     );
+
+    this.menuIds = [];
+    this.entries = JSON.parse(
+      document.querySelector('script[type="application/json"]').textContent
+    );
+    this.innerHTML = '';
+
+    this.shadowRoot.querySelector('#fabric-docs-sidebar').innerHTML += `
+    <h3 class="text-12 text-gray-500 mt-12 px-8" style="font-weight: 100;">${
+      this.entries.category.toUpperCase() || ''
+    }</h3>
+    <ul>
+    ${this.entries.items
+      .map((i) => {
+        const id = Math.random().toString(36).substr(2, 9);
+        this.menuIds.push(id);
+        this.entries.items[this.entries.items.indexOf(i)].id = id;
+
+        return `<li class="my-4">
+          <a id="${id}" ${
+          i.open ? 'aria-controls="' + id + '-child-list"' : ''
+        } tabindex="${
+          i.open ? -1 : 0
+        }" class="w-full inline-flex align-center hover:bg-gray-200 text-14 text-gray-700 font-bold py-6 px-8" style="border-radius: 4px; text-decoration: none;" role="link" aria-expanded="${
+          i.open
+        }" target="_self">${i.title}</a>
+          ${
+            i.items
+              ? `
+          <div id="${id}-child" class="${
+                  i.open ? 'block' : 'hidden'
+                }" aria-hidden="${!i.open}">
+            <ul id="${id}-child-list">
+              ${i.items
+                .map(
+                  (i) =>
+                    `<li><a class="w-full inline-flex align-center hover:bg-gray-200 font-light text-14 text-gray-700 py-6 pl-16" style="border-radius: 4px; text-decoration: none;" target="_self">${i.title}</a></li>`
+                )
+                .join('')}
+            </ul>
+          </div>`
+              : ''
+          }
+        </li>`;
+      })
+      .join('')}
+    </ul>
+    `;
+
+    for (const i in this.menuIds) {
+      const id = this.menuIds[i];
+
+      this.shadowRoot.getElementById(id).addEventListener('click', () => {
+        const e = this.entries.items.filter((e) => e.id === id);
+        console.log(e);
+      });
+    }
   }
 }
 
-class FabricDocsSidebarSection extends HTMLElement {
-  constructor() {
-    super();
-
-    const fabricStylesTemplate = document.createElement("template");
-    fabricStylesTemplate.innerHTML = `
-            <style>:host { display: block; }</style>
-            <link
-                rel="stylesheet"
-                type="text/css"
-                href="https://assets.finn.no/pkg/@finn-no/fabric-css/v0/fabric.min.css"
-            />
-            <section>
-              <slot name="title"></slot>
-              <slot name="items"></slot>
-            </section>
-        `;
-
-    this.attachShadow({ mode: "open" }).appendChild(
-      fabricStylesTemplate.content
-    );
-  }
-}
-
-if (!customElements.get("f-docs-sidebar")) {
-  customElements.define("f-docs-sidebar", FabricDocsSidebar);
-}
-
-if (!customElements.get("f-docs-sidebar-section")) {
-  customElements.define("f-docs-sidebar-section", FabricDocsSidebarSection);
+if (!customElements.get('f-docs-sidebar')) {
+  customElements.define('f-docs-sidebar', FabricDocsSidebar);
 }
