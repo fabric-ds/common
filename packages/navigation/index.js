da08f6aa032fc2eba58bd6693357f3de23db34e6
Snorre Nygaard <snorren.nygaard@finn.no>
2021-09-17 09:42:38 +0200

    Added a main template example

--- packages/navigation/index.js
+++ packages/navigation/index.js
@@ -10,16 +10,14 @@ class FabricDocsNavigation extends HTMLElement {
                 type="text/css"
                 href="https://assets.finn.no/pkg/@finn-no/fabric-css/v0/fabric.min.css"
             />
-            <nav class="flex">
-                <h1 class="h4"><img src="logo url here" alt="" />Fabric</h1>
-                <ul class="flex">
-                    <li><a class="px-24 py-16 text-gray-700" href="https://opensource.finn.no/fabric-design">Design</a></li>
-                    <li><a class="px-24 py-16 text-gray-700" href="https://opensource.finn.no/fabric-css">CSS</a></li>
-                    <li><a class="px-24 py-16 text-gray-700" href="https://opensource.finn.no/fabric-react">React</a></li>
-                    <li><a class="px-24 py-16 text-gray-700" href="https://opensource.finn.no/fabric-vue">Vue</a></li>
-                    <li><a class="px-24 py-16 text-gray-700" href="https://opensource.finn.no/fabric-elements">Elements</a></li>
-                </ul>
-            </nav>
+            <h2 class="h4 mb-0 pt-16 pb-0 pl-32 lg:hidden">Fabric sites</h2>
+            <ul class="lg:flex mb-24 lg:mb-0 pl-24 lg:pl-16 pt-4 lg:pt-0 mx-auto" style="max-width:1024px">
+              <li><a href="https://opensource.finn.no/fabric-design" class="inline-block text-gray-900 px-16 lg:py-16">Design</a></li>
+              <li><a href="https://opensource.finn.no/fabric-css" class="inline-block text-gray-900 px-16 lg:py-16">CSS</a></li>
+              <li><a href="https://opensource.finn.no/fabric-react" class="inline-block text-gray-900 px-16 lg:py-16">React</a></li>
+              <li><a href="https://opensource.finn.no/fabric-vue" class="inline-block text-gray-900 px-16 lg:py-16">Vue</a></li>
+              <li><a href="https://opensource.finn.no/fabric-elements" class="inline-block text-gray-900 px-16 lg:py-16">Elements</a></li>
+            </ul>
         `;
 
     this.attachShadow({ mode: "open" }).appendChild(
