5483340940a21f66b20273c03d27cba48236304a
Richard <digitalsadhu@gmail.com>
2021-09-24 14:22:03 +0200

    chore: update publish

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf dist;
+
+mkdir -p dist/asset-links;
+mkdir -p dist/navigation;
+mkdir -p dist/sidebar;
+mkdir -p dist/template;
+
+cp packages/navigation/index.js dist/navigation/index.js;
+cp packages/asset-links/index.js dist/asset-links/index.js;
+cp packages/sidebar/index.js dist/sidebar/index.js;
+cp packages/template/index.js dist/template/index.js;
