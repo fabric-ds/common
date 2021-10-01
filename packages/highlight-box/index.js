fe20bbfa05a464a6abaa6536ae1fb01e88aee653
Richard <digitalsadhu@gmail.com>
2021-10-01 10:53:41 +0200

    feat: add highlight box

---
+++ packages/highlight-box/index.js
@@ -0,0 +1,27 @@
+class FabricDocsHighlightBox extends HTMLElement {
+  constructor() {
+    super();
+
+    const fabricStylesTemplate = document.createElement('template');
+    fabricStylesTemplate.innerHTML = `
+      <style>:host { display: block; }</style>
+      <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://assets.finn.no/pkg/@fabric-ds/css/v0/fabric.min.css"
+      />
+      <div class="bg-yellow-100 rounded-8 p-24 mb-24">
+        <h3 class="h4">About Fabric Beta v1.0</h3>
+        <p class="pb-0">Fabric is currently in Beta phase. We’re working hard to get things running smoothly and can’t do it without your help. Head to the <a href="https://sch-chat.slack.com/archives/C01GYKPJVFT">#finn-fabric</a> channel on Slack to share your feedback.</p>
+      </div>
+    `;
+
+    this.attachShadow({ mode: 'open' }).appendChild(
+      fabricStylesTemplate.content
+    );
+  }
+}
+
+if (!customElements.get('f-docs-highlight-box')) {
+  customElements.define('f-docs-highlight-box', FabricDocsHighlightBox);
+}
