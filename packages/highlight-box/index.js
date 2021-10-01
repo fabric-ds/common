b00c861fb1911386e86e7ce9baa0bb9860bef89f
Benjamin Akar <benjaminakar2001@gmail.com>
2021-10-01 12:40:43 +0200

    Make button small

--- packages/highlight-box/index.js
+++ packages/highlight-box/index.js
@@ -13,7 +13,7 @@ class FabricDocsHighlightBox extends HTMLElement {
       <div class="bg-yellow-100 rounded-8 p-24 mb-24">
         <h3 class="h4">About Fabric Beta v1.0</h3>
         <p class="pb-0">Fabric is currently in Beta phase. We’re working hard to get things running smoothly and can’t do it without your help. Head to the <a href="https://sch-chat.slack.com/archives/C01GYKPJVFT">#finn-fabric</a> channel on Slack to share your feedback.</p>
-        <div class="mb-8"><a href="https://sch-chat.slack.com/archives/C01GYKPJVFT" class="button button--secondary">Post feedback on Slack</a></div>
+        <div class="mt-12"><a href="https://sch-chat.slack.com/archives/C01GYKPJVFT" class="button button--secondary button--small">Post feedback on Slack</a></div>
       </div>
     `;
 
